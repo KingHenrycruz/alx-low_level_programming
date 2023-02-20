01c50b62c0964d016fe388447316b35cf2978de1
KingHenrycruz <KingHenrycruz@gmail.com>
2023-02-19 16:29:54 -0800

    Write a program that prints all possible combinations of single-digit numbers, seperated by commas and whitespace

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets in lowercase followed by a new line
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+{
+	putchar(num + '0');
+	if (num == 0)
+		continue;
+	putchar(',');
+	putchar(' ');
+}
+	putchar('\n');
+	return (0);
+}
