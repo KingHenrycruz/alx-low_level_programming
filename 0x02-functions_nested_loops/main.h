a4ebe5a8b999590cbf08dfc0472bf83a49bf22a1
KingHenrycruz <KingHenrycruz@gmail.com>
2023-02-24 07:07:01 -0800

    Write a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,4 @@
 
-/**
- * Desc: My header file
- */
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -17,4 +14,3 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#end
