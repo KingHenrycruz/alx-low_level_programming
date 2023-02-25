666e753ea7d6e07b9e5ce567b72917f8c8fe0316
KingHenrycruz <KingHenrycruz@gmail.com>
2023-02-25 11:49:50 -0800

    Print a square with #

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,12 +7,20 @@
 
 void diagonal_line(int n)
 {
-	int len;
+	int len, space;
 
 	if (n > 0)
 	{
 		for (len = 0; len < n; len++)
+		{
+			for (space = 0; space < len; space ++)
+				_putchar(' ');
 			_putchar('\\');
+
+			if (len == n - 1)
+				continue;
+			_putchar('\\');
+		}
 	}
 	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square with 3
+ * @size: Size of square
+ */
+
+void print_square(int size)
+{
+	int heigt, width;
+
+	if (size > 0)
+	{
+		for (height = 0; height < size; height++)
+		{
+			for (width = 0; width < len; width++)
+				_putchar('£');
+
+			if (height == n - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
