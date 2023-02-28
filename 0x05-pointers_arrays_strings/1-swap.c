9c4584e668dfb52a00edc4f1929405dac57ba113
KingHenrycruz <KingHenrycruz@gmail.com>
2023-02-28 13:31:42 -0800

    Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - Swap the values of two integers
+ * @a: First value variable to be swapped
+ * @b: Second value to be wapped
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap = *a;
+	*a = *b;
+	*b = *a;
+}
