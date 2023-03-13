115ddb0f7408d840e0237bd69548e5e59d9e6841
KingHenrycruz <KingHenrycruz@gmail.com>
2023-03-12 17:25:54 -0700

    A program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,10 @@
  * main - prints it's name followed by a new line
  * @argv: Argument vector
  * @argc: Argument count
+ * Return: Always 0
  */
 
-int main (__attribute__ ((unused)) int argc, char *argv[])
+int main(__attribute__ ((unused)) int argc, char *argv[])
 {
 	int i;
 
